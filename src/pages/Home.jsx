78ac9d2aeed8828ac4966a9416f45e932db40878
Sheeraz Ahmed <134777088+absolutechy@users.noreply.github.com>
2023-08-04 14:51:51 +0500

    Add files via upload

---
+++ src/pages/Home.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import EmailIcon from "@mui/icons-material/Email";
+import GithubIcon from "@mui/icons-material/GitHub";
+import "../styles/Home.css";
+
+function Home() {
+  return (
+    <div className="home">
+      <div className="about">
+        <h2> Hi, My Name is Sheeraz</h2>
+        <div className="prompt">
+          <p>A software developer with a passion for learning and creating.</p>
+          <LinkedInIcon />
+          <EmailIcon />
+          <GithubIcon />
+        </div>
+      </div>
+      <div className="skills">
+        <h1> Skills</h1>
+        <ol className="list">
+          <li className="item">
+            <h2> Front-End</h2>
+            <span>
+              ReactJS, Redux, HTML, CSS, React Native, NPM,
+              BootStrap, MaterialUI, TailwindCSS
+            </span>
+          </li>
+          <li className="item">
+            <h2>Back-End</h2>
+            <span>
+              NodeJS, Java Spring, .NET, ExpressJS, GraphQL,
+              MySQL, MongoDB, DynamoDB,
+            </span>
+          </li>
+          <li className="item">
+            <h2>Languages</h2>
+            <span>JavaScript, Java, Python, C#, C, C++, TypeScript, Go</span>
+          </li>
+        </ol>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
